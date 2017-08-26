d72e4d2e7a20c0d3f7963824d7a8b552dd7e0db7
Isyraf Zakil <mohdzakilhussin.1@osu.edu>
2017-08-26 09:17:17 -0400

    added user book association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170826121502) do
+ActiveRecord::Schema.define(version: 20170826131104) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170826121502) do
     t.boolean "availability", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
